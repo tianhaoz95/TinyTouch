4f3615896e6dbacf83eea95415c916f6a393d555
Tianhao Zhou <tianhaoz@Tianhaos-MacBook-Air-7.local>
2026-09-08 14:55:21 -0700

    Boost app version to 1.2 (build 4)

--- TinyTouchiOS/Sources/Views/ParentDashboardView.swift
+++ TinyTouchiOS/Sources/Views/ParentDashboardView.swift
@@ -434,7 +434,7 @@ struct ParentDashboardView: View {
             .background(Color.green.opacity(0.1))
             .cornerRadius(14)
             
-            Text("TinyTouch v1.1 • Connected Apple Watch Companion")
+            Text("TinyTouch v1.2 • Connected Apple Watch Companion")
                 .font(.caption2)
                 .foregroundColor(.secondary)
                 .padding(.bottom, 16)
